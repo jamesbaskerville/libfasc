ed51d116ba3f528d19173cb42f42d0fbccfe9dab
James Baskerville <jameswbaskerville@gmail.com>
2019-10-27 16:34:31 -0700

    Updates

---
+++ precache-manifest.d7bf1dc56a4db0aa38c30346698cdac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2300fb0b90dec854b55994e4d762f81d",
+    "url": "/libfasc/index.html"
+  },
+  {
+    "revision": "864447e95cc9c7899f7a",
+    "url": "/libfasc/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "540cf51becc91dbcf145",
+    "url": "/libfasc/static/js/2.dcadd3c3.chunk.js"
+  },
+  {
+    "revision": "864447e95cc9c7899f7a",
+    "url": "/libfasc/static/js/main.37cd79a7.chunk.js"
+  },
+  {
+    "revision": "800ef4481bc47fac8ce3",
+    "url": "/libfasc/static/js/runtime-main.68cfb477.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/libfasc/static/media/logo.25bf045c.svg"
+  }
+]);
